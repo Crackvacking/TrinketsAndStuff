6f82839754ce4c0b7e0252756d385d2da04b47b5
Mallvac <milosz.chwedoruk@gmail.com>
2023-05-26 00:19:00 +0200

    wand added + got GeckoLib to work

---
+++ src/main/java/org/crackvacking/trinketsandstuff/item/tools/wand.java
@@ -0,0 +1,35 @@
+package org.crackvacking.trinketsandstuff.item.tools;
+
+import net.minecraft.item.Item;
+import software.bernie.geckolib3.GeckoLib;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.util.GeckoLibUtil;
+
+public class wand extends Item implements IAnimatable {
+    public AnimationFactory factory = GeckoLibUtil.createFactory(this);
+
+    public wand(Settings settings) {
+        super(settings);
+    }
+
+    private <E extends IAnimatable>PlayState predicate(AnimationEvent<E> event){
+        event.getController().setAnimation(new AnimationBuilder().addAnimation("wand.loop"));
+        return PlayState.CONTINUE;
+    }
+
+    @Override
+    public void registerControllers(AnimationData animationData) {
+        animationData.addAnimationController(new AnimationController(this, "controller", 0, this::predicate));
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return this.factory;
+    }
+}
