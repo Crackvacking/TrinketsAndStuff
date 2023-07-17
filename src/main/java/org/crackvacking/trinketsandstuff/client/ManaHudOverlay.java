f1538b5548d3078547eb94f17bff947ae942ac0d
TheTroQ <wojtek.dratwinski@gmail.com>
2023-07-17 19:57:09 +0200

    We fixed Our ManaHud display code

--- src/main/java/org/crackvacking/trinketsandstuff/client/ManaHudOverlay.java
+++ src/main/java/org/crackvacking/trinketsandstuff/client/ManaHudOverlay.java
@@ -5,16 +5,13 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.MinecartItem;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.util.collection.DefaultedList;
 import org.crackvacking.trinketsandstuff.util.IEntityDataSaver;
 import org.crackvacking.trinketsandstuff.util.ManaControll;
 
-import java.util.List;
+import static org.crackvacking.trinketsandstuff.Registry.ModItems.wand;
 
 public class ManaHudOverlay implements HudRenderCallback {
     public static final Identifier MANA_HUD = new Identifier("trinketsandstuff", "textures/gui/manabar_anim.png");
@@ -43,8 +40,8 @@ public class ManaHudOverlay implements HudRenderCallback {
         };
 
 
-        //Dynamic HUD handling
-        int displacement = MinecraftClient.getInstance().player.getInventory().offHand.toString().equals("[1 air]") ? 40 : 11;
+        //Dynamic HUD handling™
+        int displacement = MinecraftClient.getInstance().player.getStackInHand(Hand.OFF_HAND).isEmpty() ? 40 : 11;
 
         //Define texture
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
@@ -52,7 +49,7 @@ public class ManaHudOverlay implements HudRenderCallback {
         RenderSystem.setShaderTexture(0, MANA_HUD);
 
         //Texture drawing
-        if (MinecraftClient.getInstance().player.getInventory().getMainHandStack().toString().equals("1 wand")) {
+        if (MinecraftClient.getInstance().player.getStackInHand(Hand.MAIN_HAND).getItem()== wand){
             DrawableHelper.drawTexture(matrixStack, x - 197 + displacement, y - 19, 123, 3, 58, 18, 180, 396);
             DrawableHelper.drawTexture(matrixStack, x - 197 + displacement, y - 19, 64, 3+(22*frame), ManaControll.getScaledManaProgress((IEntityDataSaver) MinecraftClient.getInstance().player), 18, 180, 396);
             DrawableHelper.drawTexture(matrixStack, x - 200 + displacement, y - 22, 0, 0, 62, 22, 180, 396);
